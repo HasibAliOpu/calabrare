0397fb8cfb9a80283ff3a3f12e0a328f71693167
H_A _Opu <wwwhasib4@gmail.com>
2022-08-16 19:09:10 +0600

    setup swiper, tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#75e062",
+
+          secondary: "#b78dd6",
+
+          accent: "#f7628a",
+
+          neutral: "#222C35",
+
+          "base-100": "#F9F9FB",
+
+          info: "#37B4CD",
+
+          success: "#16A255",
+
+          warning: "#EBC21E",
+
+          error: "#E97667",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
