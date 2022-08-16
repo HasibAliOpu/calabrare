b044712011b97d1b98a56a117259b75e758c872b
H_A _Opu <wwwhasib4@gmail.com>
2022-08-16 23:38:12 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dancing: ["Dancing Script, cursive"],
+      },
+    },
+  },
   daisyui: {
     themes: [
       {
